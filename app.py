8ea51d9163d9a1aa66cb5316ea2ad05f5e39cd0c
Jorge Dantas <jatad.tech@gmail.com>
2026-02-05 22:23:29 -0300

    Aula01

--- app.py
+++ app.py
@@ -1,1 +1,16 @@
-print ("Olá Mundo!")
+#Criar ambiente virtual no Python: python -m venv venv
+#Ativar ambiente virtual:           .\venv\Scripts\activate
+#Instalar o Flask:                    pip install Flask
+
+# importando a classe Flask do framework flask
+from flask import Flask
+
+# criando a aplicação
+app = Flask(__name__)
+
+# criando a rota(URL)
+@app.route('/')
+
+# criando o recurso
+def home():
+    return "Hello, world!"
